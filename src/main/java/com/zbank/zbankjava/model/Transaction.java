6a62d2888deace84b1709e54f5286a6681bf0640
scalabreseGD <47219719+scalabreseGD@users.noreply.github.com>
2025-07-31 10:49:02 +0200

    Create Transaction entity

---
+++ src/main/java/com/zbank/zbankjava/model/Transaction.java
@@ -0,0 +1,100 @@
+package com.zbank.zbankjava.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    private String type; // e.g., DEPOSIT, WITHDRAWAL, TRANSFER
+    private LocalDateTime timestamp;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "source_account_id")
+    private Account sourceAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "target_account_id")
+    private Account targetAccount;
+
+    public Transaction() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public Transaction(BigDecimal amount, String type, String description, Account sourceAccount, Account targetAccount) {
+        this.amount = amount;
+        this.type = type;
+        this.description = description;
+        this.sourceAccount = sourceAccount;
+        this.targetAccount = targetAccount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Account getSourceAccount() {
+        return sourceAccount;
+    }
+
+    public void setSourceAccount(Account sourceAccount) {
+        this.sourceAccount = sourceAccount;
+    }
+
+    public Account getTargetAccount() {
+        return targetAccount;
+    }
+
+    public void setTargetAccount(Account targetAccount) {
+        this.targetAccount = targetAccount;
+    }
+}
