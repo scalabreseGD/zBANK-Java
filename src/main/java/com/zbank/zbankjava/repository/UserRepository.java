c1a67499ca71e3fa352e6d43b8d5cd5cdf7d9e53
scalabreseGD <47219719+scalabreseGD@users.noreply.github.com>
2025-07-31 10:49:19 +0200

    Add UserRepository interface

---
+++ src/main/java/com/zbank/zbankjava/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zbank.zbankjava.repository;
+
+import com.zbank.zbankjava.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
