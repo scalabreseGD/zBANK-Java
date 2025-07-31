3d618be3a8aa90a2300f300db2f73bc90907c909
scalabreseGD <47219719+scalabreseGD@users.noreply.github.com>
2025-07-31 10:48:20 +0200

    Create ZbankJavaApplication.java

---
+++ src/main/java/com/zbank/zbankjava/ZbankJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zbank.zbankjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbankJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbankJavaApplication.class, args);
+	}
+
+}
